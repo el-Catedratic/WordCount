55aa7c0d4f06a82e2632f88d5473f9a8d2ec55ab
el-Catedratic <akhil9167782208@gmail.com>
2023-10-08 13:43:57 +0530

    Implemented with option -c / -help

--- WordCount.java
+++ WordCount.java
@@ -4,15 +4,20 @@ public class WordCount {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		if(args.length != 1) {
-			 System.out.println("Usage: java WordCount <file>");
+		if(args.length != 2) {
+			 System.out.println("Usage: wordcount -c <file>");
 	            System.exit(1);
 		}
 		else {
-			String fileName = args[0];
+			String fileName = args[1];
 			File f = new File(fileName);
-			System.out.println(f.length() +" "+ fileName);
-			System.out.println("Next line" + f.getPath());
+			if(args[0].equals("-c")){
+				System.out.println(f.length() +" "+ fileName);
+			}
+			else if(args[0].equals("-help")){
+				System.out.println("Usage: wordcount -c <file>");
+			}
+			//System.out.println("Next line" + f.getPath());
 		}
 	}
 
